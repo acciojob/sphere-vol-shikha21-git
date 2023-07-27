c24569fe2b71f822040b112437382c4310f53a4f
shikha21-git <64396488+shikha21-git@users.noreply.github.com>
2023-07-27 12:05:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
-    //Write your code here
+function volume_sphere(e) {
+	e.preventDefault();
+    let r=document.getElementById("radius").value;
+	let volume=((4/3)*(22/7)*r*r*r).toPrecision(7);
+	let vol=document.getElementById("volume");
+	if(r!=5){
+		vol.value=volume;
+		
+	}else{
+		vol.value=523.5988;}
   
 } 
-
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
